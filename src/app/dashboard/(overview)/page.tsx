1698a91f4c6a05f0f425a3634a625bf90b522d37
roritopra <1193102598@u.icesi.edu.co>
2025-03-26 10:14:58 -0500

    refactor(dashboard): update layout and tooltip content
    - Adjust DashboardPage layout to change PieChartsWrapper grid configuration
    - Update tooltip content in TableCellRenderer from "Editar cliente" to "Editar caso" for clarity

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -17,8 +17,7 @@ export default async function DashboardPage() {
           <CardCountsWrapper />
         </Suspense>
       </div>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-2">
-        <PieChartsWrapper />
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-1">
         <PieChartCases />
       </div>
     </main>
