3c95d99dfde968f657306bceb933b41756fed81a
roritopra <1193102598@u.icesi.edu.co>
2025-03-26 10:35:39 -0500

    refactor(cases): improve TopContent component structure and add active filters check
    - Reorganize import statements for better readability
    - Introduce hasActiveFilters logic to determine if any filters are active
    - Update reset filters button to be disabled when no active filters are present

--- src/components/cases/TopContent.tsx
+++ src/components/cases/TopContent.tsx
@@ -2,7 +2,15 @@ import {
   MagnifyingGlassIcon,
   ChevronDownIcon,
 } from "@heroicons/react/24/outline";
-import { DateRangePicker, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Button, Input } from "@heroui/react";
+import {
+  DateRangePicker,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Button,
+  Input,
+} from "@heroui/react";
 import { I18nProvider } from "@react-aria/i18n";
 import { statusOptions } from "@/constants/casesConstants";
 import { DateRange, RangeValue } from "@/types/sharedTypes";
@@ -38,7 +46,6 @@ export default function TopContent({
   setStatusFilter,
   onResetFilters,
 }: TopContentProps) {
-
   // Convert dateRange to RangeValue<CalendarDate>
   const convertToDateValue = (
     dateRange: DateRange | null
@@ -58,6 +65,13 @@ export default function TopContent({
       ),
     };
   };
+
+  // Verificar si hay filtros activos
+  const hasActiveFilters = Boolean(
+    filterValue || // Filtro de búsqueda
+    (statusFilter instanceof Set && statusFilter.size > 0) || // Filtro de estado
+    (dateRange && dateRange.start && dateRange.end) // Filtro de fecha
+  );
 
   return (
     <div className="flex flex-col">
@@ -104,12 +118,18 @@ export default function TopContent({
             label="Buscar por fecha"
             className="max-w-xs"
             value={convertToDateValue(dateRange)}
-            onChange={(value) => handleDateRangeChange(value as RangeValue<CalendarDate>)}
+            onChange={(value) =>
+              handleDateRangeChange(value as RangeValue<CalendarDate>)
+            }
           />
         </I18nProvider>
         <div>
-          <Button color="primary" onPress={onResetFilters}>
-            Mostrar todos
+          <Button
+            color="primary"
+            onPress={onResetFilters}
+            isDisabled={!hasActiveFilters}
+          >
+            Limpiar filtros
           </Button>
         </div>
       </div>
